10bb5cb8cb61b95ace8afbd0e9b3bea7c53f84e1
jhoffman1204 <james.a.hoffman@stonybrook.edu>
2018-04-17 14:46:31 -0400

    i dont think i changed anything

--- test_forms/cse_305.py
+++ test_forms/cse_305.py
@@ -4,17 +4,22 @@ from text_form import SubmissionForm, LoginForm
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'somesecretkey'
 
+@app.route('/home', methods=['GET', 'POST'])
+def homepage():
+    return render_template('homepage.html', title='HomePage')
+    
 
 @app.route('/', methods=['GET', 'POST'])
 def login():
     form = LoginForm()
     if form.validate_on_submit():
-        username = form.username.data
-        password = form.password.data
-        email = form.email.data
-        return "Welcome, " + username + " / " + password + " / " + email
+#        username = form.username.data
+#        password = form.password.data
+#        email = form.email.data
+        return homepage()
     return render_template('login.html', title='Login', form=form)
 
 
 if __name__ == '__main__':
     app.run()
+
