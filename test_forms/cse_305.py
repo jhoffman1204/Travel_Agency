30a45226be8533dfa0dcacc206ed888189e980a2
EdwardRozario219 <edwardrozario12@gmail.com>
2018-04-24 17:26:16 -0400

    Logging in and account creation cases all covered

--- test_forms/cse_305.py
+++ test_forms/cse_305.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect
+from flask import Flask, render_template, request, flash
 from text_form import SubmissionForm, LoginForm, UserSignupForm
 from db_manager import dbm
 
@@ -12,14 +12,46 @@ def homepage():
 
 @app.route('/', methods=['GET', 'POST'])
 def signup():
-    form = UserSignupForm()
-    if form.validate_on_submit():
-        password = form.password.data
-        email = form.email.data
-        x = dbm.add_new_user(email, password)
-        if x:
-            return homepage()
-    return render_template('edwardHTML/login-register.html', title='Signup', form=form)    
+    #  POST REQUEST #
+    if request.method=='POST':
+        print ("POSTING")
+        # Registering
+        if "confirm-password" in request.form:
+#            print("Checking passwords")
+            username = request.form['username']
+            password = request.form['password']
+            passwordConfirm = request.form['confirm-password']
+#            print("Password: " + password)
+#            print("Password Confirm: " + passwordConfirm)
+            # Add to database
+            if password == passwordConfirm:
+                value = dbm.add_new_user(username,password)
+                if value:
+                    flash('Account created successfully')
+                else:
+                    flash('Username in use')
+                return render_template('edwardHTML/login-register.html', title='Signup')
+            # Error
+            else:
+                flash('Passwords do not match', 'failure')
+                return render_template('edwardHTML/login-register.html', title='Signup')
+        # Logging in 
+        else:
+            value = dbm.does_user_exist()
+            if value == 1:
+                return render_template('homepage.html', title='Homepage')
+            elif value == 0:
+                flash('Username does not exist')
+                return render_template('edwardHTML/login-register.html', title='Signup')
+            elif value == -1:
+                flash('Incorrect password')
+                return render_template('edwardHTML/login-register.html', title='Signup')
+            else:
+                flash('Unexpected error. Please try again')
+                return render_template('edwardHTML/login-register.html', title='Signup')     
+    # GET REQUEST #
+    else:
+        return render_template('edwardHTML/login-register.html', title='Signup')
 
 @app.route('/login', methods=['GET', 'POST'])
 def login():
--- test_forms/db_manager.py
+++ test_forms/db_manager.py
@@ -10,22 +10,30 @@ cursor = connection.cursor()
 class dbm:
     def print_test():
         print("testing")
-    def add_new_user(email, password):
-        # Checks to make sure username is valid
-        x = cursor.execute("SELECT COUNT(*) FROM Users WHERE email = '" + email + "';")
-        if x > 0:
-            print("Username in use")
-            return False
         
-        # If username is valid create a new user
-        print("new user created with username " , email 
-             , "and email " , password)
-        cursor.execute("INSERT INTO Users(email,password) VALUES ('" + email + "','" + password + "')")
-        
-        print(cursor.execute("SELECT * FROM Users"))
-        row = cursor.fetchone()
+    def print_all_users():
+        cursor.execute("SELECT * FROM Users")
+        row = cursor.fetchall()
         while row:
             print(row)
             row = cursor.fetchone()
             first_table = row
-        return True
+
+    def add_new_user(email, password):
+        dbm.print_all_users()
+        # Checks to make sure username is valid
+        x = cursor.execute("SELECT email FROM Users WHERE email = '" + email + "';") 
+        if len(cursor.fetchall()) > 0:
+            return False
+        else:
+            cursor.execute("INSERT INTO Users(email,password) VALUES ('" + email + "','" + password + "')")
+            return True
+
+    def does_user_exist(email, password):
+        x = cursor.execute("SELECT email FROM Users WHERE email = '" + email + "';") 
+        if len(cursor.fetchall()) > 0:
+            user = cursor.fetchone()
+            return True
+        else:
+            cursor.execute("INSERT INTO Users(email,password) VALUES ('" + email + "','" + password + "')")
+            return False
