867f601d8be7fa122a66f3ae08f50a6c5ce392c8
jkunzmann41 <jamiekunzmann@gmail.com>
2018-04-28 11:20:30 -0400

    added account to every page

--- test_forms/cse_305.py
+++ test_forms/cse_305.py
@@ -79,15 +79,15 @@ def car_rentals():
 
 @app.route('/flights', methods=['GET', 'POST'])
 def flights():
-	flights={'name':'flight1', 'name2':'flight2'}
-	return render_template('flights.html', title='Flights', flight=flights)
+	flights = dbm.retrieve_flights()
+	return render_template('flights.html', title='Flights', flights=flights)
 
 @app.route('/cruises', methods=['GET', 'POST'])
 def cruises():
     return render_template('cruises.html', title='Cruises')
 
 @app.route('/profile', methods=['GET', 'POST'])
-def cruises():
+def profile():
     return render_template('profile.html', title='Profile')
 
 if __name__ == '__main__':
